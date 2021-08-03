eecab490f25436d83ef2d506535cadb736ec9c55
Claudia Vizena <claudandez@gmail.com>
2021-08-03 14:33:06 -0700

    Create backend API

--- app.js
+++ app.js
@@ -1,17 +1,77 @@
 const express = require('express');
-const path = require('path');
-require('dotenv').config();
-
-console.log(process.env)
+// const path = require('path');
+const fetch = require('node-fetch');
 
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'))
-app.get('/', function(request, response){
-    response.sendFile('/public/index.html');
-});
+app.use(express.json({ limit: '1mb' }));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App'
+    })
+})
+
+app.get('/weather/:cityname', async (request,response) => {
+const cityname = request.params.cityname;
+const city = cityname.split(" ").join('+')
+const api_url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=e827cc18a30d7b0beb99903a8635c4d2`
+const fetch_response = await fetch(api_url);
+const json = await fetch_response.json();
+response.json(json);
+const  location   = json.name;
+const { icon, description } = json.weather[0];
+const { temp, humidity } = json.main;
+const { speed } = json.wind;
 
 
+console.log(location, icon, description, temp, humidity,speed)
+});
+
+
+
+
+
+
+
+
+// const express = require('express');
+// // const path = require('path');
+// const fetch = require('node-fetch');
+
+// require('dotenv').config();
+
+// const app = express();
+// const port = 3000;
+
+// app.use(express.static('public'))
+// app.get('/', function(request, response){
+//     response.sendFile('/public/index.html');
+// });
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+// // const api_url = `https://api.openweathermap.org/data/2.5/weather?q=" + 
+// // city + 
+// // "&units=metric&appid=" + 
+// // ${apiKey}`
+
+// // const response = await fetch(api_url);
+// // const json = await response.json();
+// // console.log(json)
+
+// app.get('/weather/:cityname', async (request,response) => {
+//     console.log(request.params);
+//     // const api_url = https://api.openweathermap.org/data/2.5/weather?q=" + 
+//     // city + 
+//     // "&units=metric&appid=" + 
+//     // "e827cc18a30d7b0beb99903a8635c4d2"
+// const cityname = request.params.cityname.split(" ");
+// const city = cityname.join('+')
+// const api_url = `https://api.openweathermap.org/data/2.5/weather?q=tokyo&units=metric&appid=e827cc18a30d7b0beb99903a8635c4d2`
+// const fetch_response = await fetch(api_url);
+// const json = await fetch_response.json();
+// response.json(json);
+// });
--- public/script.js
+++ public/script.js
@@ -1,35 +1,85 @@
-const { apiKey } = require("../config");
+var fetchWeather = "/weather";
+const button = document.querySelector(".search button")
+const textBox = document.querySelector(".search-bar")
 
-let weather = {
-apiKey,
-   fetchWeather: function (city) {  
-       fetch(
-           "https://api.openweathermap.org/data/2.5/weather?q=" + 
-                city + 
-                "&units=metric&appid=" + 
-                `"${apiKey}"`
-        )
-        .then((response) => response.json())
-       .then((data) => this.displayWeather(data))
-   },
-   displayWeather: function (data) {
-       const { name } = data;
-       const { icon, description } = data.weather[0];
-       const { temp, humidity } = data.main;
-       const { speed } = data.wind;
-       const arr = description.split(" ");
-       for (var i = 0; i < arr.length; i++) {
-        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1)
-       }
-       const str2 = arr.join(" ");
+const place = document.querySelector(".city");
+const icon = document.querySelector(".icon");
+const details = document.querySelector(".description");
+const temp = document.querySelector(".temp");
+const humidity = document.querySelector(".humidity");
+const wind = document.querySelector(".wind");
 
-       console.log(name,icon,description,temp,humidity,speed);
-       document.querySelector(".city").innerText = "Weather in " + name;
-       document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + ".png ";
-       document.querySelector(".description").innerText = str2;
-       document.querySelector(".temp").innerText = temp + "°C ";
-       document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-       document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
+button.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log(textBox.value);
+    place.textContent = "Searching...";
+    temp.textContent = "";
+    details.textContent= "";
+    const api_url = fetchWeather + "?address=" + textBox.value;
+    async () => {
+       const response = await fetch(api_url);
+       const json = await response.json(); 
+       console.log(json)
+    } 
+})
+
+
+// let weather, main, wind;
+// try {
+//     async function fetchWeather() {
+//         cityname = document.querySelector(".search-bar").value 
+//         const api_url = `/weather/${cityname}`
+//         const response = await fetch(api_url);
+//         const json = await response.json()
+//         const cityname = json(data)     
+//     }
+// } catch (error) {
+//     console.error(error);
+//     name = { value: ""};
+//     document.querySelector(".search-bar").textContent = "Enter a valid city"
+// }
+
+
+
+  
+
+
+
+
+
+
+    
+
+// let weather = {
+// apiKey,
+//    fetchWeather: function (city) {  
+//        fetch(
+//            "https://api.openweathermap.org/data/2.5/weather?q=" + 
+//                 city + 
+//                 "&units=metric&appid=" + 
+//                 `"${apiKey}"`
+//         )
+//         .then((response) => response.json())
+//        .then((data) => this.displayWeather(data))
+//    },
+//    displayWeather: function (data) {
+//        const { name } = data;
+//        const { icon, description } = data.weather[0];
+//        const { temp, humidity } = data.main;
+//        const { speed } = data.wind;
+//        const arr = description.split(" ");
+//        for (var i = 0; i < arr.length; i++) {
+//         arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1)
+//        }
+//        const str2 = arr.join(" ");
+
+//        console.log(name,icon,description,temp,humidity,speed);
+//        document.querySelector(".city").innerText = "Weather in " + name;
+//        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + ".png ";
+//        document.querySelector(".description").innerText = str2;
+//        document.querySelector(".temp").innerText = temp + "°C ";
+//        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+//        document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
        
-   }
-};
+//    }
+// };
